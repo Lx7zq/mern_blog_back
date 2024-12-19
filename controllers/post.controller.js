426c02e18f3efd0c3fc0d44a03d3c9ebd92341a7
Lx7zq <thisforworkonly7@gmail.com>
2024-12-19 12:39:12 +0700

    createpost

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -4,5 +4,30 @@ require("dotenv").config();
 const secret = process.env.SECRET;
 
 exports.createPost = async (req, res) => {
-  const token = req.headers["x-access-token"];
+  // file upload
+  const { path } = req.file;
+  const author = req.userId;
+  const { title, summary, content } = req.body;
+
+  // Validate input fields
+  if (!title || !summary || !content) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  try {
+    // Create post
+    const postDoc = await PostModel.create({
+      title,
+      summary,
+      content,
+      cover: path,
+      author,
+    });
+    res.json(postDoc);
+  } catch (error) {
+    // Catch and return any errors that occur during post creation
+    res
+      .status(500)
+      .json({ message: "Error creating post", error: error.message });
+  }
 };
---
+++ middlewares/file.middleware.js
@@ -0,0 +1,37 @@
+const multer = require("multer");
+const path = require("path");
+
+// Set up storage configuration
+const storage = multer.diskStorage({
+  destination: "./uploads/",
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+// Init upload
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1000000 }, // 1MB limit
+  fileFilter: (req, file, cb) => {
+    checkFileType(file, cb); // Check file extension
+  },
+}).single("file"); // input name
+
+// Function to check file type
+function checkFileType(file, cb) {
+  const fileTypes = /jpeg|jpg|png|gif|webp/;
+  const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = fileTypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb(new Error("Error: Only image files are allowed!")); // Use Error object
+  }
+}
+
+module.exports = upload;
